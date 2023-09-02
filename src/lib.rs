f3ff4c09c67bca219c9ce6b38c59800642980b93
Gungun974 <xfelix974@gmail.com>
2023-09-02 07:27:23 +0400

    Add internal error and safer API

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,34 @@ extern crate byteorder;
 extern crate lewton;
 extern crate ogg;
 
+use lewton::header::HeaderReadError;
 //use lewton::header::CommentHeader;
 use ogg::writing::PacketWriteEndInfo;
-use ogg::{Packet, PacketReader, PacketWriter};
+use ogg::{OggReadError, Packet, PacketReader, PacketWriter};
 use std::convert::TryInto;
-use std::io::{Cursor, Read, Seek};
+use std::io::{self, Cursor, Read, Seek};
+
+use thiserror::Error;
 
 pub type CommentHeader = lewton::header::CommentHeader;
+
+#[derive(Error, Debug)]
+#[error("failed to make vorbis comment")]
+pub struct VorbisMakeCommentError();
+
+#[derive(Error, Debug)]
+pub enum VorbisReadCommentError {
+    #[error("failed to read vorbis file")]
+    FailedReadOggFile(#[from] OggReadError),
+    #[error("failed to read vorbis header")]
+    FailedReadHeader(#[from] HeaderReadError),
+}
+
+#[derive(Error, Debug)]
+pub enum VorbisReplaceCommentError {
+    #[error("failed to write vorbis file")]
+    FailedReadOggFile(#[from] io::Error),
+}
 
 //type VorbisComments = CommentHeader;
 pub trait VorbisComments {
@@ -97,13 +118,16 @@ impl VorbisComments for CommentHeader {
     }
 }
 
-pub fn make_comment_header(header: &CommentHeader) -> Vec<u8> {
+pub fn safe_make_comment_header(header: &CommentHeader) -> Result<Vec<u8>, VorbisMakeCommentError> {
     //Signature
     let start = [3u8, 118, 111, 114, 98, 105, 115];
 
     //Vendor number of bytes as u32
     let vendor = header.vendor.as_bytes();
-    let vendor_len: u32 = vendor.len().try_into().unwrap();
+    let vendor_len: u32 = vendor
+        .len()
+        .try_into()
+        .map_err(|_| VorbisMakeCommentError())?;
 
     //end byte
     let end: u8 = 1;
@@ -118,7 +142,11 @@ pub fn make_comment_header(header: &CommentHeader) -> Vec<u8> {
     new_packet.extend(vendor.iter().cloned());
 
     //write number of comments
-    let comment_nbr: u32 = header.comment_list.len().try_into().unwrap();
+    let comment_nbr: u32 = header
+        .comment_list
+        .len()
+        .try_into()
+        .map_err(|_| VorbisMakeCommentError())?;
     new_packet.extend(comment_nbr.to_le_bytes().iter().cloned());
 
     let mut commentstrings: Vec<String> = vec![];
@@ -128,40 +156,56 @@ pub fn make_comment_header(header: &CommentHeader) -> Vec<u8> {
         //let commenstrings.last().as_bytes();
         let comment_len: u32 = commentstrings
             .last()
-            .unwrap()
+            .ok_or_else(|| VorbisMakeCommentError())?
             .as_bytes()
             .len()
             .try_into()
-            .unwrap();
+            .map_err(|_| VorbisMakeCommentError())?;
         new_packet.extend(comment_len.to_le_bytes().iter().cloned());
-        new_packet.extend(commentstrings.last().unwrap().as_bytes().iter().cloned());
+        new_packet.extend(
+            commentstrings
+                .last()
+                .ok_or_else(|| VorbisMakeCommentError())?
+                .as_bytes()
+                .iter()
+                .cloned(),
+        );
     }
     new_packet.push(end);
     //println!("{:?}",new_packet);
-    new_packet
+    Ok(new_packet)
+}
+
+pub fn make_comment_header(header: &CommentHeader) -> Vec<u8> {
+    safe_make_comment_header(header).unwrap()
+}
+
+pub fn safe_read_comment_header<T: Read + Seek>(
+    f_in: T,
+) -> Result<CommentHeader, VorbisReadCommentError> {
+    let mut reader = PacketReader::new(f_in);
+
+    let packet: Packet = reader.read_packet_expected()?;
+    let stream_serial = packet.stream_serial();
+
+    let mut packet: Packet = reader.read_packet_expected()?;
+    //println!("{:?}",packet.data);
+    while packet.stream_serial() != stream_serial {
+        packet = reader.read_packet_expected()?;
+        //println!("{:?}",packet.data);
+    }
+    let comment_hdr = lewton::header::read_header_comment(&packet.data)?; //println!("{:?}", comment_hdr);
+    Ok(comment_hdr)
 }
 
 pub fn read_comment_header<T: Read + Seek>(f_in: T) -> CommentHeader {
-    let mut reader = PacketReader::new(f_in);
-
-    let packet: Packet = reader.read_packet_expected().unwrap();
-    let stream_serial = packet.stream_serial();
-
-    let mut packet: Packet = reader.read_packet_expected().unwrap();
-    //println!("{:?}",packet.data);
-    while packet.stream_serial() != stream_serial {
-        packet = reader.read_packet_expected().unwrap();
-        //println!("{:?}",packet.data);
-    }
-    let comment_hdr = lewton::header::read_header_comment(&packet.data).unwrap();
-    //println!("{:?}", comment_hdr);
-    comment_hdr
+    return safe_read_comment_header(f_in).unwrap();
 }
 
-pub fn replace_comment_header<T: Read + Seek>(
+pub fn safe_replace_comment_header<T: Read + Seek>(
     f_in: T,
     new_header: CommentHeader,
-) -> Cursor<Vec<u8>> {
+) -> Result<Cursor<Vec<u8>>, VorbisReplaceCommentError> {
     let new_comment_data = make_comment_header(&new_header);
 
     let f_out_ram: Vec<u8> = vec![];
@@ -198,9 +242,7 @@ pub fn replace_comment_header<T: Read + Seek>(
                         let lastpacket = packet.last_in_stream() && packet.last_in_page();
                         let stream_serial = packet.stream_serial();
                         let absgp_page = packet.absgp_page();
-                        writer
-                            .write_packet(packet.data, stream_serial, inf, absgp_page)
-                            .unwrap();
+                        writer.write_packet(packet.data, stream_serial, inf, absgp_page)?;
                         if lastpacket {
                             break;
                         }
@@ -215,6 +257,13 @@ pub fn replace_comment_header<T: Read + Seek>(
             }
         }
     }
-    f_out.seek(std::io::SeekFrom::Start(0)).unwrap();
-    f_out
+    f_out.seek(std::io::SeekFrom::Start(0))?;
+    Ok(f_out)
+}
+
+pub fn replace_comment_header<T: Read + Seek>(
+    f_in: T,
+    new_header: CommentHeader,
+) -> Cursor<Vec<u8>> {
+    safe_replace_comment_header(f_in, new_header).unwrap()
 }
